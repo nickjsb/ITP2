d78a4cb3e742ae3c881847fa8e745dbaaa4043e6
nickjsb <51199375+nickjsb@users.noreply.github.com>
2022-10-11 22:04:12 +0100

    second commit

--- Code/flyingSaucer_1/sketch.js
+++ Code/flyingSaucer_1/sketch.js
@@ -4,14 +4,16 @@
 
 var flying_saucer_x;
 var flying_saucer_y;
+var flying_saucer_width;
 
 function setup()
 {
     createCanvas(800,600);
     noStroke();
 
-    flying_saucer_x = 200;
-    flying_saucer_y = 100;
+    flying_saucer_x = 400;
+    flying_saucer_y = 150;
+    flying_saucer_width = 200;
 }
 
 function draw()
@@ -19,15 +21,31 @@ function draw()
     background(50,0,80);
     
     //draw the ground
-    fill(0,50,0);
+    fill(0,50,  0);
     rect(0,height - 100, width, 100);
     
     //draw the flying saucer
     fill(175,238,238);
-    arc(flying_saucer_x,flying_saucer_y,75,100,PI,TWO_PI)
+    arc(flying_saucer_x, flying_saucer_y, flying_saucer_width / 2, 100, PI, TWO_PI)
     fill(150);
-    arc(flying_saucer_x,flying_saucer_y,150,50,PI,TWO_PI);
+    arc(flying_saucer_x, flying_saucer_y, flying_saucer_width, 50, PI, TWO_PI);
     fill(50);
-    arc(flying_saucer_x,flying_saucer_y,150,25,0,PI);
+    arc(flying_saucer_x, flying_saucer_y, flying_saucer_width, 25, 0, PI);
+
+    flying_saucer_x += random(-2, 2);
+    flying_saucer_y += random(-1, 1);
+
+    fill(255);
+    
+    var incr  = flying_saucer_width / (10 - 1)
+
+    for(var i = 0; i < 10; i++)
+    {
+        ellipse(flying_saucer_x - flying_saucer_width / 2 + incr * i, 
+                flying_saucer_y, 
+                7.5);
+    }
     
 }
+
+
